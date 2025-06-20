375dcd25b6bff42cccf6314d9132fc8c8e92e6b8
n_daikuji <n_daikuji@LAPTOP-R1LLA0AS>
2025-06-20 12:10:57 +0900

    コメントの一部修正

--- src/score/TestRegistExecuteController.java
+++ src/score/TestRegistExecuteController.java
@@ -30,7 +30,7 @@ public class TestRegistExecuteController extends CommonServlet {
 
 	@Override
 	protected void post(HttpServletRequest req, HttpServletResponse resp) throws Exception {
-		
+
 		// ログイン状態の確認と基本情報の取得
 		HttpSession session = req.getSession();
 		Teacher teacher = (Teacher) session.getAttribute("session_user");
@@ -46,47 +46,47 @@ public class TestRegistExecuteController extends CommonServlet {
 		// セッションから必須情報を取得
 		String subjectCd = (String) session.getAttribute("f3_selected");
 		String testNoStr = (String) session.getAttribute("f4_selected");
-		
+
 		@SuppressWarnings("unchecked")
 		List<Test> searchResults = (List<Test>) session.getAttribute("searchResults");
 
 		//必須情報がセッションにない場合はエラーとして扱う
 		if (subjectCd == null || testNoStr == null || searchResults == null) {
-			
+
 			req.setAttribute("page_error", "セッションが切れました。再度検索からやり直してください。");
 
 			//エラーの場合のドロップダウンリストのデータ取得
 			ClassNumDao classNumDaoForError = new ClassNumDao();
 			SubjectDao subjectDaoForError = new SubjectDao();
 			StudentDao studentDaoForError = new StudentDao();
-			
+
 			List<String> classListForError = classNumDaoForError.filter(school);
 			List<ClassNum> classNumListForError = new ArrayList<>();
-			
+
 			for (String classNumStrs : classListForError) {
 				ClassNum classNum = new ClassNum();
 				classNum.setClass_num(classNumStrs);
 				classNumListForError.add(classNum);
 			}
-			
+
 			List<Subject> subjectListForError = subjectDaoForError.filter(school);
 			List<Student> studentListForError = studentDaoForError.filter(school, true);
-			
+
 			req.setAttribute("classNumList", classNumListForError);
 			req.setAttribute("subjectList", subjectListForError);
 			req.setAttribute("studentList", studentListForError);
 
 			req.getRequestDispatcher("GRMU001.jsp").forward(req, resp);
-			
+
 			return;
 		}
 
 		//入力値の解析とバリデーション
 		int testNo = Integer.parseInt(testNoStr);
-		
+
 		Map<String, String> errors = new HashMap<>();
 		Map<String, String> inputValues = new HashMap<>();
-		
+
 		List<Test> testsToProcess = new ArrayList<>();
 
 		String[] deleteStudentNos = req.getParameterValues("delete_students");
@@ -94,37 +94,37 @@ public class TestRegistExecuteController extends CommonServlet {
 				: new HashSet<>();
 
 		for (Test displayedTest : searchResults) {
-			
+
 			String studentNo = displayedTest.getStudent().getNo();
 			String pointStr = req.getParameter("point_" + studentNo);
-			
+
 			inputValues.put(studentNo, pointStr);
 
 			boolean isDeleteTarget = deleteSet.contains(studentNo);
 			boolean hasPointInput = pointStr != null && !pointStr.isEmpty();
 
 			if (isDeleteTarget || hasPointInput) {
-				
+
 				Test test = new Test();
 
 				if (isDeleteTarget) {
-					
+
 					test.setToDelete(true);
-					
+
 				} else {
-					
+
 					//点数の入力値のエラー処理
 					try {
-						
+
 						int point = Integer.parseInt(pointStr);
-						
+
 						//0～100の数値以外だった場合
 						if (point < 0 || point > 100) {
 							errors.put(studentNo, "0～100で入力してください。");
 						} else {
 							test.setPoint(point);
 						}
-						
+
 						//数値以外だった場合
 					} catch (NumberFormatException e) {
 						errors.put(studentNo, "数値を入力してください。");
@@ -133,12 +133,12 @@ public class TestRegistExecuteController extends CommonServlet {
 
 				//入力情報に問題が無かった場合、データベースに値を渡すための準備をする。
 				if (!errors.containsKey(studentNo)) {
-					
+
 					Student student = new Student();
 					student.setNo(studentNo);
 					test.setStudent(student);
 					test.setClassNum(displayedTest.getClassNum());
-					
+
 					Subject subject = new Subject();
 					subject.setCd(subjectCd);
 					test.setSubject(subject);
@@ -151,7 +151,7 @@ public class TestRegistExecuteController extends CommonServlet {
 
 		// バリデーション結果に応じた処理分岐
 		if (!errors.isEmpty()) {
-			
+
 			//バリデーションエラーがあった場合
 			req.setAttribute("errors", errors);
 			req.setAttribute("inputValues", inputValues);
@@ -169,19 +169,19 @@ public class TestRegistExecuteController extends CommonServlet {
 			ClassNumDao classNumDao = new ClassNumDao();
 			SubjectDao subjectDao = new SubjectDao();
 			StudentDao studentDao = new StudentDao();
-			
+
 			List<String> classList = classNumDao.filter(school);
 			List<ClassNum> classNumList = new ArrayList<>();
-			
+
 			for (String classNumStrs : classList) {
 				ClassNum classNum = new ClassNum();
 				classNum.setClass_num(classNumStrs);
 				classNumList.add(classNum);
 			}
-			
+
 			List<Subject> subjectList = subjectDao.filter(school);
 			List<Student> studentList = studentDao.filter(school, true);
-			
+
 			req.setAttribute("classNumList", classNumList);
 			req.setAttribute("subjectList", subjectList);
 			req.setAttribute("studentList", studentList);
@@ -224,15 +224,19 @@ public class TestRegistExecuteController extends CommonServlet {
 				ClassNumDao classNumDao = new ClassNumDao();
 				SubjectDao subjectDao = new SubjectDao();
 				StudentDao studentDao = new StudentDao();
+
 				List<String> classList = classNumDao.filter(school);
 				List<ClassNum> classNumList = new ArrayList<>();
+
 				for (String classNumStrs : classList) {
 					ClassNum classNum = new ClassNum();
 					classNum.setClass_num(classNumStrs);
 					classNumList.add(classNum);
 				}
+
 				List<Subject> subjectList = subjectDao.filter(school);
 				List<Student> studentList = studentDao.filter(school, true);
+
 				req.setAttribute("classNumList", classNumList);
 				req.setAttribute("subjectList", subjectList);
 				req.setAttribute("studentList", studentList);
